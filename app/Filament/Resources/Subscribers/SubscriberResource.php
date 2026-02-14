f6d229792513d61250dc3127cefce116ef314b7d
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-02-14 16:24:47 +0000

    Fix type hints to UnitEnum to match Filament 5 parent class

--- app/Filament/Resources/Subscribers/SubscriberResource.php
+++ app/Filament/Resources/Subscribers/SubscriberResource.php
@@ -5,18 +5,18 @@ namespace App\Filament\Resources\Subscribers;
 use App\Filament\Resources\Subscribers\Pages\ListSubscribers;
 use App\Filament\Resources\Subscribers\Tables\SubscribersTable;
 use App\Models\Subscriber;
-use BackedEnum;
 use Filament\Resources\Resource;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class SubscriberResource extends Resource
 {
     protected static ?string $model = Subscriber::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedEnvelope;
+    protected static string|UnitEnum|null $navigationIcon = Heroicon::OutlinedEnvelope;
 
-    protected static string|BackedEnum|null $navigationGroup = 'Newsletter';
+    protected static string|UnitEnum|null $navigationGroup = 'Newsletter';
 
     public static function canCreate(): bool
     {
