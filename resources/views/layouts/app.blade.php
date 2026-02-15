905b89bc8b24a7752567a001e8f9325b7580e0f6
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-02-15 21:02:49 +0000

    Add JSON-LD structured data for rich snippets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     {!! seo() !!}
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @include('partials.json-ld')
     @stack('head')
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700|jetbrains-mono:400,500" rel="stylesheet" />
