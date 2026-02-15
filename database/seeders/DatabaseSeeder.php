d0f77003b9899d129d903c1995ebf22100f05ca8
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-02-15 16:45:24 +0000

    Fix seeder email to thelaravelarchitect@gmail.com

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::firstOrCreate(
-            ['email' => 'jeffrey@thelaravelarchitect.com'],
+            ['email' => 'thelaravelarchitect@gmail.com'],
             [
                 'name' => 'Jeffrey Davidson',
                 'password' => \Illuminate\Support\Facades\Hash::make('change-me-immediately'),
