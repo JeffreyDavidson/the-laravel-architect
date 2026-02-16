e39f8344d0811b20a60f5162300283427c974577
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-02-16 19:43:33 +0000

    Uses page: change terminal prompt to php artisan uses:list

--- resources/views/pages/uses.blade.php
+++ resources/views/pages/uses.blade.php
@@ -73,7 +73,7 @@
                 <div class="flex items-center gap-3 mb-4">
                     <div class="font-mono text-sm text-gray-500 flex items-center gap-2">
                         <span class="text-[#4A7FBF]">$</span>
-                        <span>cat ~/.uses</span>
+                        <span>php artisan uses:list</span>
                         <span class="animate-pulse text-gray-400 dark:text-[#4A7FBF] relative -top-px">▊</span>
                     </div>
                 </div>
