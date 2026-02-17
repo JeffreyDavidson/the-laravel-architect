2e2904f2f9030adac578760cf3c6983d3ceba407
Jeffrey Davidson <jeffreydavidson@users.noreply.github.com>
2026-02-17 01:48:25 +0000

    Projects filter tabs: smaller padding/text on mobile, fix light mode border

--- resources/views/components/projects/filter-tabs.blade.php
+++ resources/views/components/projects/filter-tabs.blade.php
@@ -10,7 +10,7 @@
         <button
             @click="filter = '{{ $filter['key'] }}'"
             :class="filter === '{{ $filter['key'] }}' ? 'active' : ''"
-            class="filter-tab px-4 py-2 text-sm font-medium rounded-lg border border-[#1e2a3a] text-gray-400 hover:text-white hover:border-gray-600 whitespace-nowrap flex-shrink-0"
+            class="filter-tab px-3 sm:px-4 py-2 text-xs sm:text-sm font-medium rounded-lg border border-gray-200 dark:border-[#1e2a3a] text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:border-gray-400 dark:hover:border-gray-600 whitespace-nowrap flex-shrink-0"
         >
             {{ $filter['label'] }}
         </button>
