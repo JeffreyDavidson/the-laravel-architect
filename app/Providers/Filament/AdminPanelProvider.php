4911e8023635af16cd5083219ade60b18b878ef8
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-02-17 15:44:17 +0000

    Remove duplicate widget registration (auto-discovered already)

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -70,9 +70,7 @@ class AdminPanelProvider extends PanelProvider
                 Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
-            ->widgets([
-                \App\Filament\Widgets\WelcomeWidget::class,
-            ])
+            ->widgets([])
             ->viteTheme('resources/css/filament/admin/theme.css')
             ->middleware([
                 EncryptCookies::class,
