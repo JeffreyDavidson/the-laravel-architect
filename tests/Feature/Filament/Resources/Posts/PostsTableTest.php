1849f0769d822751087fbb00c2f632d0577cf242
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-08-19 11:55:57 -0400

    refactor: type post table URL callback

---
+++ tests/Feature/Filament/Resources/Posts/PostsTableTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Filament\Resources\Posts\Pages\ListPosts;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use function Pest\Livewire\livewire;
+
+uses(RefreshDatabase::class);
+
+it('links the view on site action to the public post URL', function () {
+    $user = User::factory()->create(['is_admin' => true]);
+    $this->actingAs($user);
+
+    $post = Post::query()->create([
+        'title' => 'Filament table actions',
+        'slug' => 'filament-table-actions',
+        'content' => 'Post content',
+        'user_id' => $user->id,
+    ]);
+
+    livewire(ListPosts::class)
+        ->assertTableActionHasUrl('view_on_site', route('blog.show', $post), $post)
+        ->assertTableActionShouldOpenUrlInNewTab('view_on_site', $post);
+});
