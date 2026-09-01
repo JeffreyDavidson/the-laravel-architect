d9f257938acc6e189e3cbc2397dbc248e335d37e
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-01 10:37:37 -0400

    fix: redact Nightwatch request previews (#310)

--- app/Monitoring/RedactNightwatchRequest.php
+++ app/Monitoring/RedactNightwatchRequest.php
@@ -2,15 +2,22 @@
 
 namespace App\Monitoring;
 
+use Laravel\Nightwatch\Core;
 use Laravel\Nightwatch\Records\Request;
+use Laravel\Nightwatch\State\CommandState;
+use Laravel\Nightwatch\State\RequestState;
 
 final class RedactNightwatchRequest
 {
+    /** @param Core<RequestState|CommandState> $nightwatch */
+    public function __construct(private readonly Core $nightwatch) {}
+
     public function __invoke(Request $request): bool
     {
         $request->url = $request->routePath !== ''
             ? $request->routePath
             : '[unmatched route]';
+        $this->nightwatch->executionState->executionPreview = "{$request->method} {$request->url}";
         $request->ip = '';
         $request->headers->replace([]);
 
