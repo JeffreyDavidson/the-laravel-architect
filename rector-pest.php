0df428369529c356e9ae6057dfcc02a619cbb301
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-17 19:13:09 -0400

    chore: standardize html assertions (#449)

--- rector-pest.php
+++ rector-pest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Pest\Rector\Set\PestSetList;
 use Rector\Config\RectorConfig;
-use RectorLaravel\Rector\MethodCall\AssertSeeToAssertSeeHtmlRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -14,7 +13,4 @@ return RectorConfig::configure()
     ->withSets([
         PestSetList::CODING_STYLE,
     ])
-    ->withComposerBased(laravel: true)
-    ->withSkip([
-        AssertSeeToAssertSeeHtmlRector::class,
-    ]);
+    ->withComposerBased(laravel: true);
