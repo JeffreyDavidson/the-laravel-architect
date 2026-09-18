3f6d7946c55efaef50345cc4667ecede12bc0412
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-18 14:58:35 -0400

    test: allow more time for browser initialization

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,3 +4,6 @@ use Tests\TestCase;
 
 pest()->extend(TestCase::class)->in('Architecture', 'Browser', 'Feature', 'Integration');
 pest()->extend(PHPUnit\Framework\TestCase::class)->in('Unit');
+
+// Allow cold browser initialization on CI without adding fixed delays to tests.
+pest()->browser()->timeout(10000);
