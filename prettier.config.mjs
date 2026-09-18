ef3da37a18eb0fbeab4c3196abf54866c272face
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-18 17:35:34 -0400

    chore: standardize Prettier configuration

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('prettier').Config} */
 export default {
     plugins: ['prettier-plugin-blade', 'prettier-plugin-tailwindcss'],
+    semi: true,
     singleQuote: true,
-    trailingComma: 'all',
-    printWidth: 120,
+    tabWidth: 4,
+    trailingComma: 'es5',
+    printWidth: 100,
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    endOfLine: 'lf',
 };
